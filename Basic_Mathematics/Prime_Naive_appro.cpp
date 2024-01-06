4b89580cdcc04d0c6b476137cded5bc07cfe6254
Vikas <vikasbrb2002@gmail.com>
2024-01-06 10:16:48 +0530

    done

---
+++ Basic_Mathematics/Prime_Naive_appro.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    if(n<=1) return false;
+    for(int i=2;i<n;i++){
+         if(n%i==0) return false;
+    }
+    return true;
+}
+int main(){
+  int n=19;
+  int count=0;
+  for(int i=1;i<=n;i++){
+    if(isprime(i)) count++;
+  }
+  cout<<count<<endl;
+
+return 0;
+}
